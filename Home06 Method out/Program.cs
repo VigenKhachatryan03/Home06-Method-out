aa5c6856c22c3b324eb426fc0a57898e58ece184
a <a@DESKTOP-TU47UA5>
2022-12-04 23:02:56 +0400

    Add project files.

---
+++ Home06 Method out/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Home06_Method_out
+{
+    class Program
+    {
+        static int power(int a, int b, out double c)
+        {
+            int power = 1;
+            
+            while (b > 0)
+            {
+                power *= a;
+                b--;
+            }
+            c = power / 2;
+
+            return power;
+        }
+        static void Main(string[] args)
+        {
+            double a;
+            Console.WriteLine(power(9, 2, out a));
+            Console.WriteLine(a);
+        }
+    }
+}
